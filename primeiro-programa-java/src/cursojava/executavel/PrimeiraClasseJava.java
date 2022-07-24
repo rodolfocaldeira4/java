13c00d4982e395117c245e7186e1804089e509ad
rudeveloper <rudeveloper@RUDEVELOPER-LENOVO>
2022-07-24 11:09:42 -0300

    Criação

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/* meu primeiro programa em Java */
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java");
+		System.out.println("Java é orientado a objetos");
+	}
+}
